7ec4054c60be49b7cd5119098f992e300481b0f7
Durgesh <your-github-gaudd221@gmail.com>
2025-12-12 20:57:52 +0530

    Fix: Rename skillsSection to SkillsSection and update import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { ProjectSection } from "../components/ProjectSection";
-import { SkillsSection } from "../components/skillsSection";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
